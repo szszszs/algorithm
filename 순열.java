fc67ade8ff522ecf2012f5c666d86348b6bb1227
szszszs <61152781+szszszs@users.noreply.github.com>
2020-02-24 14:14:48 +0900

    Add files via upload
    순열(Permutation): 서로 다른 것들 중 몇 개를 뽑아서 한 줄로 나열하는 것
    nPr : 서로 다른 n개 중 r개를 택하는 순열
    
    재귀 호출을 통한 순열 생성

---
+++ 순열.java
@@ -0,0 +1,41 @@
+/**
+ * 	순열 Permutation
+ * @author multicampus
+ *
+ */
+public class 순열 {
+	public static int[] arr = {6,7,8};
+	
+	public static void main(String[] args) {
+		
+		int n = arr.length;
+		int r = 3;//네개중 두개 뽑고싶으면 2
+		perm(r,0);//r이 될때까지 반복해라 , n은 고정
+		
+	}//end of main
+	/** r: 뽑을 개수, k: 현재단계*/
+	public static void perm(int r, int k) {
+		if(r == k) {//종료 파트
+			for (int i = 0; i < r; i++) {
+				System.out.print(arr[i]+" ");
+			}
+			System.out.println();
+		}else {//재귀파트
+			//k 이전은 확정 k단계부터 
+			for (int i = k; i < arr.length; i++) {//0~(k-1)확정, 미선택수 k~끝
+				//arr[i] <-> arr[k] swap
+				int temp = arr[k];
+				arr[k] = arr[i];
+				arr[i] = temp;
+				perm(r,k+1);
+				////arr[i] <-> arr[k] swap,다음 작업을 위해서 원상 복귀
+				temp = arr[k];
+				arr[k] = arr[i];
+				arr[i] = temp;
+			}
+			
+		}
+		
+	}
+
+}//end of class
