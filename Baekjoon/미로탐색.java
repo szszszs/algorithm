57331827226ce303c1e56a2bbde0a692914a2128
szszszs <61152781+szszszs@users.noreply.github.com>
2020-02-28 23:21:18 +0900

    Update 미로탐색.java

--- Baekjoon/미로탐색.java
+++ Baekjoon/미로탐색.java
@@ -25,7 +25,7 @@ public class 미로탐색{
 		}//end of input
 		
 		bfs();
-		System.out.println(map[N-1][M-1]);
+		System.out.println(map[N-1][M-1]);//좌표 값을 배열의 범위로
 		
 	}//end of main
 	
@@ -42,7 +42,7 @@ public class 미로탐색{
 				if(isInside(nx,ny) && map[nx][ny]==1 && !visited[nx][ny]) {
 					q.offer(new Pair(nx,ny));
 					visited[nx][ny] = true;
-					map[nx][ny] = map[p.x][p.y] + 1;
+					map[nx][ny] = map[p.x][p.y] + 1;//이동 거리 
 					
 				}
 			}
